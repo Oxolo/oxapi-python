ddd12417d0cb211bc097942a7218ecd1e559066d
Naser Derakhshan <n.derakhshan@oxolo.com>
2022-05-25 14:11:03 +0430

    Change the PyPi package name to oxapi

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -23,7 +23,7 @@ copyright = ""
 author = "Oxolo"
 
 # The full version, including alpha/beta/rc tags
-release = "v0.0.3"
+release = "v0.0.4"
 
 # -- General configuration ---------------------------------------------------
 
