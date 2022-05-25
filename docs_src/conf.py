10af2a3cdc63366be78d7f63af1f1470221e49b3
Naser Derakhshan <n.derakhshan@oxolo.com>
2022-05-25 13:40:53 +0430

    increase version

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -23,7 +23,7 @@ copyright = ""
 author = "Oxolo"
 
 # The full version, including alpha/beta/rc tags
-release = "v0.0.2"
+release = "v0.0.3"
 
 # -- General configuration ---------------------------------------------------
 
