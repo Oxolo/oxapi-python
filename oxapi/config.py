f8b897db010e9c0b717f4ce4c950caf12528788f
Adriano Strinati <a.strinati@oxolo.com>
2022-06-06 08:56:23 +0200

    Updated requirements, updated version

--- oxapi/config.py
+++ oxapi/config.py
@@ -1,4 +1,4 @@
 log_level = "DEBUG"
-version = "0.0.6"
+version = "0.0.7"
 default_api_version = "v1"
 default_model_version = "v1"
--- setup.py
+++ setup.py
@@ -8,7 +8,12 @@ from setuptools import setup
 
 sys.path.append("./oxapi/")
 
+from pathlib import Path
+
 from config import version
+
+this_directory = Path(__file__)
+long_description = (this_directory / "README.md").read_text()
 
 NAME = "oxapi"
 VERSION = version
@@ -16,7 +21,14 @@ DESCRIPTION = ""
 AUTHOR = ""
 AUTHOR_EMAIL = ""
 PACKAGES = ["oxapi", "oxapi.nlp", "oxapi.abstract"]
-INSTALL_REQUIRES = ["grequests>=0.6.0", "numpy>=1.17", "pandas>=1.3"]
+INSTALL_REQUIRES = [
+    "grequests>=0.6.0",
+    "numpy>=1.17",
+    "pandas>=1.3",
+    "requests==2.20",
+    "urllib3==1.24.3",
+    "jinja2==2.11.3",
+]
 
 setup(
     name=NAME,
@@ -26,4 +38,6 @@ setup(
     author_email=AUTHOR_EMAIL,
     packages=PACKAGES,
     install_requires=INSTALL_REQUIRES,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
